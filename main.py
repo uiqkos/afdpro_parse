b3a66dc5841a2b77728a1ba73722bcda9390a971
uiqkos <57287053+uiqkos@users.noreply.github.com>
2019-11-28 19:40:01 +0300

    #2 first work ver with excel integration windows

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import re
+import openpyxl
 
 # arg 0 is name
 # arg 1 is path to trace file
-#
+# arg 2 is path to excel file
+
 file = open(sys.argv[1], 'r')
+
 lines = file.readlines()[3:-2]
 
 commands = []
@@ -12,11 +15,11 @@ i = 0
 
 while i in range(len(lines)):
     commands.append(''.join(lines[i:i + 4]))
-    i +=4
+    i += 4
 
 reg_values = []
 flag_values = []
-current_address_and_command = []
+curr_adr_com = []
 
 registers = [
     'AX', 'SI', 'CS', 'Stack +0',
@@ -24,17 +27,37 @@ registers = [
     'CX', 'BP', 'ES', 'Stack +4',
     'DX', 'SP', 'SS', 'Stack +6'
 ]
+registers = dict().fromkeys(registers)
+for i, reg in enumerate(registers.keys()):
+    registers[reg] = i
+
 flags = [
     'OF', 'DF', 'IF', 'SF', 'ZF', 'AF', 'PF', 'CF'
 ]
 
 for i, command in enumerate(commands):
-    current_address_and_command.append(re.findall('\w+', command[:13]))
+    curr_adr_com.append(re.findall('\w+', command[:13]))
     reg_values.append(re.findall('[0-9A-F]{4}', command[42:]))
     flag_values.append(re.findall('[0-1]', command[-63:-40]))
 
+# for i in range(len(reg_values)):
+#     for k, reg in enumerate(reg_values[i]):
+#         print(registers[k], '=', reg)
+#     for k, flag in enumerate(flag_values[i]):
+#         print(flags[k], '=', flag)
+
+excel_file = openpyxl.load_workbook(filename = 'pp.xlsx')
+sheet = excel_file['test']
+
 for i in range(len(reg_values)):
-    for k, reg in enumerate(reg_values[i]):
-        print(registers[k], '=', reg)
-    for k, flag in enumerate(flag_values[i]):
-        print(flags[k], '=', flag)
+    for i in range(1, len(curr_adr_com)):
+        sheet.cell(row=i, column=1).value = curr_adr_com[i][0]
+        sheet.cell(row=i, column=2).value = curr_adr_com[i][1]
+        sheet.cell(row=i, column=3).value = reg_values[i][registers['AX']]
+        sheet.cell(row=i, column=4).value = reg_values[i][registers['BX']]
+        sheet.cell(row=i, column=5).value = reg_values[i][registers['CX']]
+        sheet.cell(row=i, column=6).value = reg_values[i][registers['DX']]
+        for k, flag in enumerate(flag_values[i]):
+            sheet.cell(row=i, column=7+k).value = flag
+excel_file.save('pp.xlsx')
+file.close()

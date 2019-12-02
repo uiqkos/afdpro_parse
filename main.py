7b7197acfd1a6089052952f1ce04e70c69ae2e57
uiqkos <57287053+uiqkos@users.noreply.github.com>
2019-12-02 18:20:59 +0300

    #6

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ import openpyxl
 # arg 3 tab from left
 # arg 4 tab from top
 
-def parse_afdpro(trace_file_path, excel_file_path, tab_top, tab_left, mask=['curr_addr']):
-    registers = [
+registers = [
         'AX', 'SI', 'CS', 'Stack +0',
         'BX', 'DI', 'DS', 'Stack +2',
         'CX', 'BP', 'ES', 'Stack +4',
         'DX', 'SP', 'SS', 'Stack +6'
-    ]
-    flags = [
+]
+flags = [
         'OF', 'DF', 'IF', 'SF', 'ZF', 'AF', 'PF', 'CF'
-    ]
+]
 
-    class Iteration:
-        def __init__(self, reg_values, flags_values, curr_addr, command):
-            self.regs = dict()
-            self.flags = dict()
-            self.curr_addr = curr_addr
-            self.command = command
-            for i, reg in enumerate(registers):
-                self.regs[reg] = reg_values[i]
-            for i, flag in enumerate(flags):
-                self.flags[flag] = flags_values[i]
+class Iteration:
+    def __init__(self, reg_values, flags_values, curr_addr, command):
+        self.regs = dict()
+        self.flags = dict()
+        self.curr_addr = curr_addr
+        self.command = command
+        for i, reg in enumerate(registers):
+            self.regs[reg] = reg_values[i]
+        for i, flag in enumerate(flags):
+            self.flags[flag] = flags_values[i]
 
-        def __getattr__(self, item):
-            if item in registers:
-                return self.regs[item]
-            if item in flags:
-                return self.flags[item]
-            return self.__dict__[item]
+    def __getattr__(self, item):
+        if item in registers:
+            return self.regs[item]
+        if item in flags:
+            return self.flags[item]
+        return self.__dict__[item]
 
+def parse_afdpro(trace_file_path, excel_file_path, tab_top, tab_left, mask=['curr_addr']):
     file = open(trace_file_path, 'r')
     lines = file.readlines()[3:-2]
     commands = [''.join(lines[i:i + 4]) for i in range(0, len(lines), 4)]
@@ -46,10 +46,10 @@ def parse_afdpro(trace_file_path, excel_file_path, tab_top, tab_left, mask=['cur
     for i, command in enumerate(commands):
         temp = re.findall('\w+', command[:12])
         iterations.append(Iteration(
-            re.findall('[0-9A-F]{4}', command[42:]),
-            re.findall('[0-1]', command[-63:-40]),
-            temp[0],
-            temp[1]
+            reg_values   = re.findall('[0-9A-F]{4}', command[42:]), # hex number ignore first 42 chars
+            flags_values = re.findall('[0-1]', command[-63:-40]), # 0 or 1
+            curr_addr    = temp[0],
+            command      = temp[1]
         ))
 
     excel_file = openpyxl.load_workbook(filename = excel_file_path)
